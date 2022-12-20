8ebac2bed1074f0cdb84fe940fc89e9fd7216e9c
Regendra <regendrasuman2017@gmail.com>
2022-12-20 10:18:12 +0530

    Get Current Date time

---
+++ android/app/src/main/kotlin/com/example/currentdatetime/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.currentdatetime
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home:  MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var currentDateTime;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    currentDateTime = DateTime.now(); //Get Current Date time Function
+    super.initState();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+appBar: AppBar(
+  title: Text("Current Date time"),
+),
+      body: Center(
+        child: Text("Current Time is ${currentDateTime} "),
+      ),
+    );
+  }
+}
